6ca98179457967b7dc77d1ad74ceefc3fc8644fa
Aditya-Tolikar <adityatolikar@gmail.com>
2021-10-07 01:24:26 +0530

    Update index_script.js

--- assets/index_script.js
+++ assets/index_script.js
@@ -1,7 +1,7 @@
 function changeTheme()
 {
 	var currentStyle = document.getElementById("styler").href;
-	if (currentStyle === "file:///C:/Users/Ulterno/Documents/WORK/Website/assets/index_stylesheet_black.css")
+	if (currentStyle === "https://github.com/Aditya-Tolikar/Aditya-Tolikar.github.io/blob/main/assets/index_stylesheet_black.css")
 	{
 		document.getElementById("styler").href = "./assets/index_stylesheet_white.css";
 	}
