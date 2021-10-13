48e6d50cec722ece7b335af11ce6c7bdca5a70a7
Aditya-Tolikar <adityatolikar@gmail.com>
2021-10-13 10:07:48 +0530

    Update index_script.js

--- assets/index_script.js
+++ assets/index_script.js
@@ -7,7 +7,7 @@ function SetPreferredColourScheme()
 		}
 	}
 	document.documentElement.setAttribute("data-theme", currentTheme);
-}SetPreferredColourScheme();
+}
 
 function changeTheme()
 {
@@ -22,3 +22,37 @@ function changeTheme()
 		document.getElementsByClassName('themechanger')[0].setAttribute("title", "Light Theme");
 	}
 }
+
+function ScrollNavBar()
+{
+	var navbar_height = document.getElementById("navbar").offsetHeight;
+	document.documentElement.style.setProperty("--JS-calculated-navbar-height", navbar_height/2 + 'px');
+	
+	/*var header_height = document.getElementsByClassName('header')[0].offsetHeight;*/
+	if (window.pageYOffset >= document.getElementById("header").offsetHeight)
+	{
+		/*document.getElementById("navbar").style.position = "fixed";
+		document.getElementById("navbar").style.top = 0;*/
+		document.getElementById("navbar").classList.remove("below-header");
+		document.getElementById("navbar").classList.add("fixed-on-top");
+	}
+	else
+	{
+		/*document.getElementById("header").style.position = "reative";*/
+		document.getElementById("navbar").classList.remove("fixed-on-top");
+		document.getElementById("navbar").classList.add("below-header");
+	}
+}
+
+function StartX()
+{
+	SetPreferredColourScheme();
+	if (window.addEventListener)
+	{
+		window.addEventListener ("scroll", function() {ScrollNavBar();});
+	}
+	else if (window.attachEvent)
+	{
+		window.attachEvent("onscroll", function() {ScrollNavBar();});
+	}
+}StartX();
